8a74365ff819d3a2f66fa0b6d9521fa1c67c90f3
Alex Norman <alex@x37v.info>
2021-02-19 16:40:04 -0800

    set bpm via properties
    NOTE only goes to link when transport is active

--- src/JackTransportLink.cpp
+++ src/JackTransportLink.cpp
@@ -1,5 +1,12 @@
 #include "JackTransportLink.hpp"
 
+#include <jack/uuid.h>
+
+namespace {
+  const char * decimal_type = "https://www.w3.org/2001/XMLSchema#decimal";
+  const std::string(bpm_key) = "http://www.x37v.info/jack/metadata/bpm";
+}
+
 JackTransportLink::JackTransportLink(
     jack_client_t * client,
     bool enableStartStopSync,
@@ -10,14 +17,17 @@ JackTransportLink::JackTransportLink(
 ) :
   mJackClient(client),
   mBPM(initialBPM),
+  mBPMLast(initialBPM),
   mInitialQuantum(initialQuantum),
   mInitialTimeSigDenom(initialTimeSigDenom),
   mInitialTicksPerBeat(initialTicksPerBeat),
-  mLink(initialBPM)
+  mLink(initialBPM),
+  mJackClientUUID(0)
 {
   //setup link
   mLink.setTempoCallback([this](double bpm) {
     mBPM.store(bpm, std::memory_order_release);
+    setBPMProperty(bpm);
   });
   if (enableStartStopSync) {
     mLink.setStartStopCallback([this](bool isPlaying) {
@@ -30,6 +40,17 @@ JackTransportLink::JackTransportLink(
   }
   mLink.enableStartStopSync(enableStartStopSync);
   mLink.enable(true);
+
+  //intialize our bpm property
+  {
+    //try to get our uuid, if we can get it, we set the property and property callback
+    char * uuids;
+    if ((uuids = jack_get_uuid_for_client_name(mJackClient, jack_get_client_name(mJackClient))) != nullptr
+        && jack_uuid_parse(uuids, &mJackClientUUID) == 0) {
+      setBPMProperty(mBPM.load(std::memory_order_acquire));
+      jack_set_property_change_callback(mJackClient, JackTransportLink::propertyChangeCallback, this);
+    }
+  }
 
   //setup jack, become the timebase master, unconditionally
   jack_set_process_callback(mJackClient, JackTransportLink::processCallback, this);
@@ -88,20 +109,26 @@ void JackTransportLink::timeBaseCallback(jack_transport_state_t transportState, 
   double bpm = mBPM.load(std::memory_order_acquire);
   double quantum = bbtValid ? pos->beats_per_bar : mInitialQuantum;
 
-  if (posIsNew && bbtValid) {
-    //update tempo
-    bpm = pos->beats_per_minute;
-    sessionState.setTempo(bpm, mTime); 
+  bool commit = false;
 
+  //see if we've had a bpm update
+  if (bpm != mBPMLast) {
+    commit = true;
+    sessionState.setTempo(bpm, mTime);
+    mBPMLast = bpm;
+  }
+
+  if (posIsNew && bbtValid) {
+    commit = true;
     //request position
     double linkBeat = static_cast<double>(pos->bar - 1) * quantum
       + static_cast<double>(pos->beat - 1)
       + static_cast<double>(pos->tick) / pos->ticks_per_beat;
     //TODO offset time based on bbtOffset?
     sessionState.requestBeatAtTime(linkBeat, mTime, quantum);
-
-    mLink.commitAudioSessionState(sessionState);
   }
+  if (commit)
+    mLink.commitAudioSessionState(sessionState);
 
   auto linkBeat = std::max(0.0, sessionState.beatAtTime(mTime, quantum));
   auto linkPhase = sessionState.phaseAtTime(mTime, quantum);
@@ -132,4 +159,39 @@ int JackTransportLink::syncCallback(jack_transport_state_t state, jack_position_
 int JackTransportLink::syncCallback(jack_transport_state_t /*transportState*/, jack_position_t * /*pos*/) {
   //TODO delay start to sync with time from session?
   return 1;
+}
+
+void JackTransportLink::propertyChangeCallback(jack_uuid_t subject, const char *key, jack_property_change_t change, void *arg) {
+  return reinterpret_cast<JackTransportLink *>(arg)->propertyChangeCallback(subject, key, change);
+}
+
+void JackTransportLink::propertyChangeCallback(jack_uuid_t subject, const char *key, jack_property_change_t change) {
+  //if the subject is all or us and the key is all (empty) or bpm
+  if ((jack_uuid_empty(subject) || subject == mJackClientUUID) && (!key || bpm_key.compare(key) == 0)) {
+    if (change == jack_property_change_t::PropertyChanged) {
+      char * values = nullptr;
+      char * types = nullptr;
+      if (0 == jack_get_property(mJackClientUUID, bpm_key.c_str(), &values, &types)) {
+        //convert to double and store if success
+        char* pEnd = nullptr;
+        double bpm = std::strtod(values, &pEnd);
+        if (*pEnd == 0)
+          mBPM.store(bpm, std::memory_order_release);
+        //free
+        if (values)
+          jack_free(values);
+        if (types)
+          jack_free(types);
+      }
+    } else if (change == jack_property_change_t::PropertyDeleted) {
+      setBPMProperty(mBPM.load(std::memory_order_acquire));
+    }
+  }
+}
+
+void JackTransportLink::setBPMProperty(double bpm) {
+  if (!jack_uuid_empty(mJackClientUUID)) {
+    std::string bpms = std::to_string(bpm);
+    jack_set_property(mJackClient, mJackClientUUID, bpm_key.c_str(), bpms.c_str(), decimal_type);
+  }
 }
--- src/JackTransportLink.hpp
+++ src/JackTransportLink.hpp
@@ -5,6 +5,7 @@
 
 #include <jack/jack.h>
 #include <jack/types.h>
+#include <jack/metadata.h>
 
 #include <ableton/Link.hpp>
 #include <ableton/link/HostTimeFilter.hpp>
@@ -25,10 +26,13 @@ class JackTransportLink {
     static int processCallback(jack_nframes_t nframes, void *arg);
     static void timeBaseCallback(jack_transport_state_t state, jack_nframes_t nframes, jack_position_t *pos, int new_pos, void *arg);
     static int syncCallback(jack_transport_state_t state, jack_position_t *pos, void *arg);
+    static void propertyChangeCallback(jack_uuid_t subject, const char *key, jack_property_change_t change, void *arg);
   private:
     int processCallback(jack_nframes_t nframes);
     void timeBaseCallback(jack_transport_state_t state, jack_nframes_t nframes, jack_position_t *pos, bool posIsNew);
     int syncCallback(jack_transport_state_t state, jack_position_t *pos);
+    void propertyChangeCallback(jack_uuid_t subject, const char *key, jack_property_change_t change);
+    void setBPMProperty(double bpm);
 
     jack_client_t * mJackClient;
     ableton::Link mLink;
@@ -39,7 +43,10 @@ class JackTransportLink {
     jack_transport_state_t mTransportStateReportedLast = jack_transport_state_t::JackTransportStopped;
 
     std::atomic<double> mBPM;
+    double mBPMLast;
     double mInitialQuantum; //time sig num, called quantum in link
     float mInitialTimeSigDenom;
     double mInitialTicksPerBeat;
+
+    jack_uuid_t mJackClientUUID;
 };
